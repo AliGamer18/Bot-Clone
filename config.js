e47a6a6d25021e4bb51513e240df3f967746129e
AliGamer18 <mohamedmahmoudtube2@gmail.com>
2025-01-08 18:27:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports={
-    probot_ids :'',//أيدي بروبوت
-    recipientId : '',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
-    price : '',//سعر النسخ
-    serverid : '',//أيدي سيرفارك
-    token : ''//توكن بوتك
+    probot_ids :'282859044593598464',//أيدي بروبوت
+    recipientId : '1282291977509011507',//أيدي الشخص ألي يتحول له الكريدت عندا نسخ السيرفر
+    price : '5000',//سعر النسخ
+    serverid : '1326204503002845255',//أيدي سيرفارك
+    token : 'MTMyMDM5ODg1OTkxODI0NTg4OA.GALFfV._k0F_gaT_ir54Jn8Ds6HBFwaWNYA20Jjjd6xwk'//توكن بوتك
 }
